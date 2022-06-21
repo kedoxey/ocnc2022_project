42f0db04c32baa30048c58c88cc4e80f1712696b
kedoxey <kedoxey@gmail.com>
2022-06-21 12:12:15 +0900

    validate calculated vs simulated spiking rates of individual neurons

--- spiking_SSN.py
+++ spiking_SSN.py
@@ -4,18 +4,20 @@ import scipy as sp
 import scipy.signal
 import numpy as np
 import brian2
-from brian2.units import ms, mV, second
+from brian2.units import ms, mV, second, Hz, kHz
 from brian2 import check_units
+import matplotlib.pyplot as plt
 
 # Parameters
 N_E = 40
 N_I = 10
+N = N_E + N_I
 tau_E = 20*ms  # ms
 tau_I = 10*ms  # ms
 V_rest = -70*mV  # mV
 V_0 = -70*mV  # mV
 n = 2
-k = 0.3*(mV**-n)/2  # mV^(-n)*s^(-1)
+k = 0.3*(mV**-n)/second  # mV^(-n)*s^(-1)
 W_EE = 1.25*mV*second  # mV*s, E -> E
 W_IE = 1.2*mV*second  # mV*s, E -> I
 W_EI = 0.65*mV*second  # mV*s, I -> E
@@ -33,8 +35,30 @@ l_stim = 60  # deg
 b = 2*mV  # mV
 A_max = 20*mV  # mV
 theta_stim = 0  # deg
-dt = 0.1*ms  # ms
+time_step = 0.1 * ms  # ms
 c = 1  # contrast
+
+
+# Pre-synaptic Connections for each neuron i
+def get_pre_syn_conns(neuron_type):
+    if neuron_type == 'E':
+        return np.random.randint(0, N, int(P_E*N_E))
+    else:
+        return np.random.randint(0, N, int(P_I*N_I))
+
+
+C = np.zeros((N, N))
+for i in range(0, N):
+    pre_syn_E = get_pre_syn_conns('E')
+    pre_syn_I = get_pre_syn_conns('I')
+
+    temp = 5
+    for e_idx in pre_syn_E:
+        C[e_idx, i] = 1
+    for i_idx in pre_syn_I:
+        C[i_idx, i] = 1
+
+C_sources, C_targets = C.nonzero()
 
 # Synaptic Weights (J_ij) for j to i
 J_EE = W_EE/(tau_syn*P_E*N_E)  # E -> E
@@ -42,15 +66,28 @@ J_EI = W_EI/(tau_syn*P_I*N_I)  # I -> E
 J_IE = W_IE/(tau_syn*P_E*N_E)  # E -> I
 J_II = W_II/(tau_syn*P_I*N_I)  # I -> I
 
-# Pre-synaptic Connections for each neuron i
-E_neurons = np.random.random_integers(0, N_E, int(P_E*N_E))
-I_neurons = np.random.random_integers(0, N_I, int(P_I*N_I))
+J = np.zeros((N,N))
+J[0:N_E,0:N_E] = J_EE*C[0:N_E,0:N_E]
+J[0:N_E,N_E:N] = J_EI*C[0:N_E,N_E:N]
+J[N_E:N,0:N_E] = J_IE*C[N_E:N,0:N_E]
+J[N_E:N,N_E:N] = J_II*C[N_E:N,N_E:N]
+
+J_sources, J_targets = J.nonzero()
+weights = J.flatten()
 
 
 # Momentary Spiking Rate
-@check_units(v=mV)
+@check_units(v=mV,result=Hz)
 def r(v):
-    return (k*(np.floor((v-V_0)/mV)*mV)**n) if v-V_0 > 0 else 0
+    rate = (k*((np.floor((v-V_0)/mV)*mV)**n))
+
+    if v.size > 1:
+        for rate_idx, rate_idv in enumerate(rate):
+            rate[rate_idx] = rate_idv if v[rate_idx]-V_0 > 0 else 0*Hz
+    else:
+        rate = rate if v-V_0 > 0 else 0*Hz
+
+    return rate
 
 
 # Mean External Input
@@ -87,8 +124,45 @@ da/dt = -a/tau_syn + sum_delta(t_j-axon_delay) : 1
 a_tot_post = J_ij*a : 1 (summed)
 '''
 
-test_rate = r(30*mV)/dt
-test_delta = axon_delay(5 * ms)
+# Stochastic Firing Probability
+threshold = 'rand() < time_step*r(v)'
+
+# Validate Firing Rates
+
+test_voltages = np.arange(-80,-50,1)  #
+test_rates = [r(test_voltage*mV)/Hz for test_voltage in test_voltages]
+
+plt.figure(1)
+plt.plot(test_voltages,test_rates)
+plt.xlabel('membrane potential (mV)')
+plt.ylabel('instantaneous firing rate (Hz)')
+plt.xticks(np.arange(-80,-40,10))
+plt.title('Calculated')
+plt.show()
+
+fixed_Vm_eqs = '''
+v : volt
+'''
+
+fixed_G = brian2.NeuronGroup(len(test_voltages), fixed_Vm_eqs, threshold='rand() < time_step*r(v)', reset='v = V_rest', dt=time_step)
+fixed_G.v = test_voltages*mV
+fixed_M = brian2.SpikeMonitor(fixed_G, variables='v', record=True)
+
+brian2.run(1*second)
+
+voltages = fixed_M.v
+spike_trains = fixed_M.spike_trains()
+spike_counts = fixed_M.count
+spike_rates = spike_counts/time_step
+all_values = fixed_M.all_values()
+
+plt.figure(2)
+plt.plot(test_voltages, list(spike_rates))
+plt.xlabel('membrane potential (mV)')
+plt.ylabel('firing rate (Hz)')
+plt.xticks(test_voltages)
+plt.title('Simulated')
+plt.show()
 
 temp = 5
 
